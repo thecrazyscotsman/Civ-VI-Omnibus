a207c37e05f5e315dc77627adf1f6495b7896869
thecrazyscotsman <mwsewall@gmail.com>
2016-12-19 13:48:10 -0500

    Update UserSettingUpdates.sql

--- Configuration/UserSettingUpdates.sql
+++ Configuration/UserSettingUpdates.sql
@@ -120,13 +120,18 @@ UPDATE GlobalParameters
 SET Value = 4
 WHERE Name = 'CITY_MIN_RANGE'
 AND EXISTS (SELECT * FROM Omnibus_UserSettings WHERE Setting = 'TCS_MIN_CITY_RANGE' AND Toggle = 1);
+
 UPDATE Buildings
 SET RegionalRange = RegionalRange + 1
-WHERE RegionalRange > 0 AND EXISTS (SELECT * FROM Omnibus_UserSettings WHERE Setting = 'TCS_MIN_CITY_RANGE' AND Toggle = 1);
+WHERE RegionalRange > 0 
+AND EXISTS (SELECT * FROM Omnibus_UserSettings WHERE Setting = 'TCS_MIN_CITY_RANGE' AND Toggle = 1);
+
 UPDATE GlobalParameters SET Value = (SELECT Value FROM GlobalParameters WHERE Name = 'CITY_MIN_RANGE') + 7 
 WHERE Name = 'RELIGION_SPREAD_ADJACENT_CITY_DISTANCE';
+
 UPDATE GlobalParameters SET Value = (SELECT Value FROM GlobalParameters WHERE Name = 'CITY_MIN_RANGE') + 6 
 WHERE Name = 'START_DISTANCE_MAJOR_CIVILIZATION';
+
 UPDATE GlobalParameters SET Value = (SELECT Value FROM GlobalParameters WHERE Name = 'CITY_MIN_RANGE') + 2 
 WHERE Name = 'START_DISTANCE_MINOR_CIVILIZATION';
 --INCREASE DISTANCE BETWEEN CITIES END
